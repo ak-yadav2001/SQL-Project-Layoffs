a5f77be8d721ae0735aa1c6563a7b28745c62d31
Ashish Kumar Yadav <78577525+ak-yadav2001@users.noreply.github.com>
2024-07-24 15:32:40 +0530

    Update Project's EDA.sql

--- Project's EDA.sql
+++ Project's EDA.sql
@@ -1,10 +1,5 @@
 -- EDA
 
--- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
-
--- normally when you start the EDA process you have some idea of what you're looking for
-
--- with this info we are just going to look around and see what we find!
 
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
